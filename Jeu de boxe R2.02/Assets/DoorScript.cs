7a50caa7872d137c9eea08837e5de831c33439d2
Cubeman74 <bellahceneyanis03@gmail.com>
2022-06-23 17:24:10 +0200

    e

--- Jeu de boxe R2.02/Assets/DoorScript.cs
+++ Jeu de boxe R2.02/Assets/DoorScript.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DoorScript : MonoBehaviour
 {
     public GameObject door;
+    bool isOpen;
+    int cnt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOpen = false;
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        if (Input.GetKey(KeyCode.K))
-            door.transform.Rotate(new Vector3(0, 0, 0));
+    {
+        if (Input.GetKeyDown(KeyCode.K) && isOpen ==  false)
+        {
+            cnt = 0;
+            while (door.transform.rotation.eulerAngles.z < 90 && cnt < 90)
+            {
+                Debug.Log(door.transform.rotation.eulerAngles.z);
+                door.transform.Rotate(new Vector3(0, 0, 1));
+                cnt++;
+            }
+            isOpen = true;
+
+        } 
+        else if(Input.GetKeyDown(KeyCode.K) && isOpen == true)
+        {
+            door.transform.Rotate(new Vector3(0, 0, -90));
+            isOpen = false;
+        }
+        
     }
+
 }
